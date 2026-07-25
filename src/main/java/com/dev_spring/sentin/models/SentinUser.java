8017574155daf6f6df1828d8b9fd5663be64b669
Patricio García Ocampo <patgo39@gmail.com>
2026-07-24 23:33:00 -0600

    feat: SentinUser model implemented

---
+++ src/main/java/com/dev_spring/sentin/models/SentinUser.java
@@ -0,0 +1,58 @@
+package com.dev_spring.sentin.models;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sentin_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SentinUser {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_user")
+  private Long idUser; // 💡 Tip: Usa el tipo envolvente 'Long' en lugar del primitivo 'long' para manejar estados transitorios (null antes de persistir).
+  
+  @Column(name = "given_name", nullable = false, length = 20)
+  private String givenName;
+
+  @Column(name = "middle_name", length = 20)
+  private String middleName;
+
+  @Column(name = "family_name", nullable = false, length = 60)
+  private String familyName;
+
+  @Column(name = "username", nullable = false, unique = true, length = 15)
+  private String username;
+
+  @Column(name = "password", nullable = false, length = 255)
+  private String password;
+
+  @Column(name = "email", nullable = false, unique = true, length = 50)
+  private String email;
+
+  @Column(name = "birth_date", nullable = false)
+  private LocalDate birthDate;
+
+  @Column(name = "postal_code", nullable = false, length = 5)
+  private String postalCode;
+
+  @Column(name = "phone_number", nullable = false, length = 10)
+  private String phoneNumber;
+
+  @Column(name = "rfc", nullable = false, length = 13)
+  private String rfc;
+  
+}
