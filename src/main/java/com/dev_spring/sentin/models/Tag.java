66d4bbdfebd7f36ae31992fd2ba75e4a0b91440e
Patricio García Ocampo <patgo39@gmail.com>
2026-07-30 19:09:21 -0600

    feat: Tag entity mapped

--- src/main/java/com/dev_spring/sentin/models/Tag.java
+++ src/main/java/com/dev_spring/sentin/models/Tag.java
@@ -1,5 +1,44 @@
 package com.dev_spring.sentin.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "tag",
+    uniqueConstraints = @UniqueConstraint(
+        name = "uq_user_tag_name",
+        columnNames = {"id_user", "tag_name"}
+    )
+)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tag {
-  
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tag")
+    private Long idTag;
+
+    @Column(name = "id_user")
+    private Long idUser;
+
+    @NotBlank
+    @Size(max = 60)
+    @Column(name = "tag_name", length = 60, nullable = false)
+    private String tagName;
 }
