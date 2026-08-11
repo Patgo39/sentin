6c00e43d2e3120f07319e524b2ad89e5d55ecde2
Patricio García Ocampo <patgo39@gmail.com>
2026-08-10 20:40:52 -0600

    feat: SentinUserRepository uses JpaSpecificationExecutor

--- src/main/java/com/dev_spring/sentin/repositories/SentinUserRepository.java
+++ src/main/java/com/dev_spring/sentin/repositories/SentinUserRepository.java
@@ -1,9 +1,12 @@
 package com.dev_spring.sentin.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.dev_spring.sentin.models.SentinUser;
 
-public interface SentinUserRepository extends JpaRepository<SentinUser, Long>{
+public interface SentinUserRepository extends JpaRepository<SentinUser, Long>, JpaSpecificationExecutor<SentinUser> {
 
+  boolean existsByUsername(String username);
+  boolean existsByEmail(String email);
 }
