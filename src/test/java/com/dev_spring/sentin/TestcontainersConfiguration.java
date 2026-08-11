23764060b991976ac679003620c7358a66c16a14
Patricio García Ocampo <patgo39@gmail.com>
2026-08-11 12:34:56 -0600

    problem in install

--- src/test/java/com/dev_spring/sentin/TestcontainersConfiguration.java
+++ src/test/java/com/dev_spring/sentin/TestcontainersConfiguration.java
@@ -3,8 +3,6 @@ package com.dev_spring.sentin;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.testcontainers.DockerClientFactory;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -13,7 +11,6 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	@ConditionalOnExpression("#{T(org.testcontainers.DockerClientFactory).instance().isDockerAvailable()}")
 	PostgreSQLContainer postgresContainer() {
 		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
 	}
