b654eb13d19f4a0973f6280a7d3ddf4e90102782
Patricio García Ocampo <patgo39@gmail.com>
2026-08-11 12:22:28 -0600

    Step 4: Make TestcontainersConfiguration conditional on Docker availability; add H2 test dependency

--- src/test/java/com/dev_spring/sentin/TestcontainersConfiguration.java
+++ src/test/java/com/dev_spring/sentin/TestcontainersConfiguration.java
@@ -3,6 +3,8 @@ package com.dev_spring.sentin;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -11,8 +13,9 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+	@ConditionalOnExpression("#{T(org.testcontainers.DockerClientFactory).instance().isDockerAvailable()}")
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
 	}
 
 }
