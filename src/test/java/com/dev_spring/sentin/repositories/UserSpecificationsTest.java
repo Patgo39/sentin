ebe2f82cad4055c8f83bda6c3bf0bff0e39b79a7
Patricio García Ocampo <patgo39@gmail.com>
2026-08-14 01:39:27 -0600

    test: UserSpecificationTest was created to test the behavior of the repository with the Specification

---
+++ src/test/java/com/dev_spring/sentin/repositories/UserSpecificationsTest.java
@@ -0,0 +1,19 @@
+package com.dev_spring.sentin.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserSpecificationsTest {
+  @Autowired
+  private SentinUserRepository userRepository;
+
+  @BeforeEach
+  void setUp() {
+    
+  }
+}
