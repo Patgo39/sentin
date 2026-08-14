ac7d1f8ebbba81ff814b27b41619312bfb6808b8
Patricio García Ocampo <patgo39@gmail.com>
2026-08-14 01:41:11 -0600

    fix: unused import deleted in UserSpecificationTest

--- src/test/java/com/dev_spring/sentin/repositories/UserSpecificationsTest.java
+++ src/test/java/com/dev_spring/sentin/repositories/UserSpecificationsTest.java
@@ -2,7 +2,6 @@ package com.dev_spring.sentin.repositories;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 
