db2981eea5655a5a31f6ad03cb36acd44141a932
Patricio García Ocampo <patgo39@gmail.com>
2026-08-24 22:23:08 -0600

    feat: SentinUserService modifications with Page and Pageable

--- src/main/java/com/dev_spring/sentin/services/SentinUserService.java
+++ src/main/java/com/dev_spring/sentin/services/SentinUserService.java
@@ -1,5 +1,21 @@
 package com.dev_spring.sentin.services;
 
-public interface SentinUserService {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable; 
 
+import com.dev_spring.sentin.dtos.ChangeEmailRequest;
+import com.dev_spring.sentin.dtos.ChangePasswordRequest;
+import com.dev_spring.sentin.dtos.CreateUserRequest;
+import com.dev_spring.sentin.dtos.UpdateUserProfileRequest;
+import com.dev_spring.sentin.dtos.UserFilterParams;
+import com.dev_spring.sentin.dtos.UserResponse;
+
+public interface SentinUserService {
+  Long registerUser(CreateUserRequest request);
+  void updateUserProfile(Long userId, UpdateUserProfileRequest request);
+  void changeUserPassword(Long userId, ChangePasswordRequest request);
+  void changeUserEmail(Long userId, ChangeEmailRequest request);
+  Page<UserResponse> filterUsers(UserFilterParams filterParams, Pageable pageable);
+  void deleteUser(Long userId);
+  UserResponse getUserById(Long userId);
 }
